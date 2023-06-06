d78b196b29f15c55c20c06cef77f30cfb72b23d9
Seif Nageh <seifldin.attallah@gmail.com>
2023-06-06 15:59:25 -0700

    Turbo

--- src/components/homePage/CustomerCompanyLogos.vue
+++ src/components/homePage/CustomerCompanyLogos.vue
@@ -1,9 +1,43 @@
+<script setup>
+import { ref } from "vue";
+
+const basicUrl = ref("/src/assets/images/customerLogos/");
+const companies = ref([
+  {
+    name: "castello",
+    url: `${basicUrl.value}castello.png`,
+  },
+  {
+    name: "saudi",
+    url: `${basicUrl.value}saudi.png`,
+  },
+  {
+    name: "golden",
+    url: `${basicUrl.value}golden.png`,
+  },
+  {
+    name: "brulee",
+    url: `${basicUrl.value}brulee.png`,
+  },
+  {
+    name: "meridien",
+    url: `${basicUrl.value}meridien.png`,
+  },
+]);
+</script>
+
 <template>
   <div class="grid grid-cols-5 gap-12 container py-8">
-    <img
-      src="@/assets/images/customerLogos/saudi.png"
-      alt="saudi"
-      class="grayscale hover:grayscale-0 transition-all duration-500"
-    />
+    <div
+      v-for="company of companies"
+      :key="company.name"
+      class="flex items-center justify-center"
+    >
+      <img
+        :src="company.url"
+        :alt="company.name"
+        class="grayscale hover:grayscale-0 transition-all duration-500"
+      />
+    </div>
   </div>
 </template>
